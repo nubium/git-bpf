dc64cefa25080a35883ee26a0bea06b312d1a923
Ondrej Vlach <ondrej.vlach@nubium.cz>
2016-10-31 16:44:36 +0100

    Remove unused parameter gitbpf.remoterecreate from init

--- lib/git_bpf/commands/init.rb
+++ lib/git_bpf/commands/init.rb
@@ -17,7 +17,6 @@ class Init < GitFlow/'init'
     opts.script_dir_name = 'git-bpf'
     opts.remote_name = 'origin'
     opts.rerere_branch = 'rr-cache'
-    opts.remote_recreate = '"*"'
     opts.remote_url = nil
 
     [
@@ -33,9 +32,6 @@ class Init < GitFlow/'init'
       ['-b', '--rerere-branch NAME',
         "",
         lambda { |n| opts.rerere_branch = n }],
-      ['-e', '--remote-recreate NAME',
-        "Pattern of branches which will be used in search for merged branches in recreated branch. Dafeaults to *",
-        lambda { |n| opts.remote_recreate = n }],
     ]
   end
 
@@ -142,7 +138,6 @@ class Init < GitFlow/'init'
 
     target.config(true, "gitbpf.remotename", opts.remote_name)
     target.config(true, "gitbpf.rerebranch", opts.rerere_branch)
-    target.config(true, "gitbpf.remoterecreate", opts.remote_recreate)
 
     rerere_path = File.join(target.git_dir, 'rr-cache')
     target_remote_url = !opts.remote_url.nil? ? opts.remote_url : target.remoteUrl(opts.remote_name)
